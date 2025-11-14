21262e516165200a46b5c93c699a08288e9d8dda
= <=>
2025-11-14 20:27:17 +0700

    add tailwind repo

---
+++ bai7-tailwind-config/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        "tuychon": "#2233CC",
+        "tuychon2": {
+          100: "22FFCC",
+          500: "22FFFF"
+        }
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
